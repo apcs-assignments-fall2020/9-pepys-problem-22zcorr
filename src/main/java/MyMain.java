5138ba9cfd614e0cf91e96c586dc6b8831108705
22zcorr <22zcorr@athenian.org>
2020-10-04 18:04:46 +0000

    Have to take a break, working on HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,78 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double sixes = 0.0;
+        for(int i =0; i<10000;i++){
+            for(int j =0;j<6;j++){
+                if((int)(Math.random()*6)+1==6){
+                sixes+=1;
+                j=0;
+                break;
+                }
+
+            }
+        }
+        double prct = sixes/100.0;
+        return prct;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double sixes = 0.0;
+        int temp6 = 0;
+        for(int i =0; i<10000;i++){
+            temp6=0;
+            for(int j =0;j<12;j++){
+
+                if(temp6==2){
+                    temp6=0;
+                    j=0;
+                    sixes+=1;
+                    break; 
+                }
+
+                if((int)(Math.random()*6)+1==6){
+                temp6+=1;  
+                }
+
+            }
+        }
+        double prct = sixes/100.0;
+        return prct;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double sixes = 0.0;
+        int temp6 = 0;
+        for(int i =0; i<10000;i++){
+            temp6=0;
+            for(int j =0;j<18;j++){
+
+                if(temp6==3){
+                    temp6=0;
+                    j=0;
+                    sixes+=1;
+                    break; 
+                    }
+
+                if((int)(Math.random()*6)+1==6){
+                temp6+=1;  
+                }
+
+            }
+        }
+        double prct = sixes/100.0;
+        return prct;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("5 runs of each:");
+        System.out.println(probabilityOneSix()+" "+probabilityOneSix()+" "+probabilityOneSix()+" "+probabilityOneSix()+" "+probabilityOneSix());
+        System.out.println(probabilityTwoSixes()+" "+probabilityTwoSixes()+" "+probabilityTwoSixes()+" "+probabilityTwoSixes()+" "+probabilityTwoSixes());
+        System.out.println(probabilityThreeSixes()+" "+probabilityThreeSixes()+" "+probabilityThreeSixes()+" "+probabilityThreeSixes()+" "+probabilityThreeSixes());
     }
 }
